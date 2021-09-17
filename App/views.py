1789269176a4aa4dfd16b285dfead14b4fefa604
G060N0FF <georgi.hr.dim@gmail.com>
2021-09-17 13:02:02 +0300

    Created base template

--- App/views.py
+++ App/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    is_auth = request.user.is_authenticated
+    
+    context = {
+        'is_auth': is_auth,
+    }
+    return render(request, 'App/index.html', context)
