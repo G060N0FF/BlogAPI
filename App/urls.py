ec0caf3dc014b3086496a64ca443ad5a0120d9bc
G060N0FF <georgi.hr.dim@gmail.com>
2021-09-17 22:11:06 +0300

    Login page

--- App/urls.py
+++ App/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('accounts/', include('django.contrib.auth.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
