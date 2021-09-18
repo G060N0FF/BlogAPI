953c105d0c7a7f287370eaa80b219865050dc164
G060N0FF <georgi.hr.dim@gmail.com>
2021-09-18 23:56:42 +0300

    Blog model

--- App/models.py
+++ App/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+blog_categories = (
+    (1, 'Technology'),
+    (2, 'Travel'),
+    (3, 'Music'),
+    (4, 'Lifestyle'),
+    (5, 'Sports'),
+    (6, 'Finance'),
+    (7, 'Political'),
+    (8, 'Business'),
+    (9, 'Personal'),
+    (10, 'Movie'),
+    (11, 'Car'),
+    (12, 'Gaming'),
+)
+
+
+class BlogPost(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, null=True)
+    title = models.CharField(max_length=30)
+    image = models.ImageField(upload_to='blog_images')
+    category = models.IntegerField(choices=blog_categories)
+    text = models.TextField()
